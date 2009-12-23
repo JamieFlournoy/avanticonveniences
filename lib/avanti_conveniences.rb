8e70fc1500b2af096dc7f91d8d416f61ab880cc2
Jamie Flournoy <jamie@seriousbusiness.com>
2009-12-22 16:27:28 -0800

    Require recently added files, oops.

--- lib/avanti_conveniences.rb
+++ lib/avanti_conveniences.rb
@@ -1,7 +1,11 @@
 require File.dirname(__FILE__) + '/arg_checks'
+require File.dirname(__FILE__) + '/array_extensions'
+require File.dirname(__FILE__) + '/die'
+require File.dirname(__FILE__) + '/float_extensions'
 require File.dirname(__FILE__) + '/hash_extensions'
 require File.dirname(__FILE__) + '/string_extensions'
 require File.dirname(__FILE__) + '/text_formatter'
+require File.dirname(__FILE__) + '/time_extensions'
 require File.dirname(__FILE__) + '/uri_extensions'
 
 class AvantiConveniences
